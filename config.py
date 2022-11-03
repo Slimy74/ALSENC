64ef0f5b074372ddd7019dc00381c03e8da260b1
Slimy74 <115116196+Slimy74@users.noreply.github.com>
2022-11-03 19:45:48 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-Token = "TOKEN"
-Prefix = '.'
-PointsName = "Goon Tokens"
-Log_channel = 821442828056002602 # Channel where all shop purchases are logged.
-POINTS_TO_BE_GIVEN_PER_MESSAGE = 2 # Points which will be given to members per message.
+Token = "MTAzNzc5NjU5Nzk0MzI0NjkwOQ.GS_v41.ldvKTG_CB17EKrlpl9gf_HWgojTNGriMfKilto"
+Prefix = '$'
+PointsName = "Solania"
+Log_channel = 1037797393829220392 # Channel where all shop purchases are logged.
+POINTS_TO_BE_GIVEN_PER_MESSAGE = 1 # Points which will be given to members per message.
